590e9e881e61ba24368ab1b321fa2ff4f3202a35
Silvur <Silvur@yandex.ru>
2022-04-14 16:00:33 +0500

    add models: Competition, Segment, Category, Nation add models to admin.py

--- skates/figureSkaters/models.py
+++ skates/figureSkaters/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Nation(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Segment(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Competition(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
